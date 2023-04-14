96103db3b56a85f1526b192bdf5fbf7875160c46
macnguyennguyenhuan <macnguyennguyenhuan@gmail.com>
2023-04-14 09:50:17 +0700

    changed css for all users

--- resources/views/auth/users.blade.php
+++ resources/views/auth/users.blade.php
@@ -3,15 +3,35 @@
 <head>
     <title>Custom Auth in Laravel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <style>
-        table, th, td {
-            border:1px solid black;
+
+
+        table{
+            width: 100%;
+            border-collapse: collapse;
+            border-radius: 1em;
+            overflow: hidden;
         }
         th{
+            border-bottom: 2px solid white;
+            background-color: #4b8bff;
+            color: #fff;
             text-align: center;
+
         }
         td{
+            border: 2px solid white;
             padding: 10px;
+            background: #ddd;
+        }
+        h1{
+            font-family: 'Poppins', sans-serif;
+            text-align: center;
+            margin-bottom: 50px;
+            font-weight: bold;
         }
     </style>
 </head>
@@ -42,7 +62,8 @@
     </div>
 </nav>
 <div class="container">
-    <table style="border: solid 1px #000">
+    <h1>All REGISTED USERS</h1>
+    <table>
         <tr>
             <th>ID</th>
             <th>Name</th>
